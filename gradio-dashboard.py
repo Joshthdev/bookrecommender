5729bc41125741975c41467c0f553dd902fcf887
Joshthdev <joshuapharez6@gmail.com>
2025-05-14 19:53:40 -0400

    launch command change

--- gradio-dashboard.py
+++ gradio-dashboard.py
@@ -104,6 +104,4 @@ with gr.Blocks(theme = gr.themes.Glass()) as dashboard:
                         outputs = output)
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
-    dashboard.launch(server_name="0.0.0.0", server_port=port)
+dashboard.launch()
